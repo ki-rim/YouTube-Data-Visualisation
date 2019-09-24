e07614a76538ad65b0454dbeb5fc1384b83c0140
Ki Rim <d.kim1423@gmail.com>
2019-09-25 00:02:31 +1200

    3rd

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 fetch("data.json")
 .then(response => response.json())
 .then(json =>{
-var keys = [];
-let selectedData = document.getElementById('selector').value;
-let graphTitle= selector.options[selector.selectedIndex].text;
-let graphTitle2= selector2.options[selector2.selectedIndex].text;
-console.log(selectedData);
-var file = eval("json.".concat(selectedData));
-console.log(file);
-var label;
-if(graphTitle.includes("Watch")){
-  label = "Videos watched";
-}
-else{
-  label= "Searches made";
-}
-let selectedData2 = document.getElementById('selector2').value;
-console.log(selectedData2);
+  var keys = [];
+  let selectedData = document.getElementById('selector').value;
+  let graphTitle= selector.options[selector.selectedIndex].text;
+  let graphTitle2= selector2.options[selector2.selectedIndex].text;//type of graph
+  let padding = 0;
+  console.log(selectedData);
+  var file = eval("json.".concat(selectedData));
+  console.log(file);
+  var label;
+  if(graphTitle.includes("Watch")){
+    label = "Videos watched";
+  }
+  else{
+    label= "Searches made";
+  }
+  let selectedData2 = document.getElementById('selector2').value;
+  console.log(selectedData2);
+  document.getElementById("wrapper").style.height = '128px';
 
-
-var names = [];
-for(var i =0;i<file.length;i++){
-  names.push(file[i].name);
-}
-var values = [];
-for(var i =0;i<file.length;i++){
-  values.push(file[i].value);
-}
+  var names = [];
+  for(var i =0;i<file.length;i++){
+    names.push(file[i].name);
+  }
+  var values = [];
+  for(var i =0;i<file.length;i++){
+    values.push(file[i].value);
+  }
+  Chart.defaults.global.defaultFontColor = 'black';
+  Chart.defaults.global.defaultFontSize = 20;
+  Chart.defaults.global.defaultFontFamily = 'Overpass Mono';
   let myChart = document.getElementById('myChart').getContext('2d');
   let dataChart = new Chart(myChart,{
 
@@ -38,25 +42,97 @@ for(var i =0;i<file.length;i++){
         data:values,
         //backgroundColor:'green',
         backgroundColor:[
-          'rgba(255, 99, 132, 0.6)',
-          'rgba(54, 162, 235, 0.6)',
-          'rgba(255, 206, 86, 0.6)',
-          'rgba(75, 192, 192, 0.6)',
-          'rgba(153, 102, 255, 0.6)',
-          'rgba(255, 159, 64, 0.6)',
-          'rgba(255, 99, 132, 0.6)'
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(75, 192, 192, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(255, 159, 64, 0.7)',
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(75, 192, 192, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(255, 159, 64, 0.7)',
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(75, 192, 192, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(255, 159, 64, 0.7)',
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(75, 192, 192, 0.7)',
+          'rgba(153, 102, 255, 0.7)',
+          'rgba(255, 159, 64, 0.7)',
+          'rgba(255, 99, 132, 0.7)'
         ],
-        borderWidth:1,
-        borderColor:'#777',
-        hoverBorderWidth:3,
-        hoverBorderColor:'#000'
+        borderWidth:3,
+        borderColor:[
+          'rgba(255, 99, 132, 0.3)',
+          'rgba(54, 162, 235, 0.3)',
+          'rgba(255, 206, 86, 0.3)',
+          'rgba(75, 192, 192, 0.3)',
+          'rgba(153, 102, 255, 0.3)',
+          'rgba(255, 159, 64, 0.3)',
+          'rgba(255, 99, 132, 0.3)',
+          'rgba(54, 162, 235, 0.3)',
+          'rgba(255, 206, 86, 0.3)',
+          'rgba(75, 192, 192, 0.3)',
+          'rgba(153, 102, 255, 0.3)',
+          'rgba(255, 159, 64, 0.3)',
+          'rgba(255, 99, 132, 0.3)',
+          'rgba(54, 162, 235, 0.3)',
+          'rgba(255, 206, 86, 0.3)',
+          'rgba(75, 192, 192, 0.3)',
+          'rgba(153, 102, 255, 0.3)',
+          'rgba(255, 159, 64, 0.3)',
+          'rgba(255, 99, 132, 0.3)',
+          'rgba(54, 162, 235, 0.3)',
+          'rgba(255, 206, 86, 0.3)',
+          'rgba(75, 192, 192, 0.3)',
+          'rgba(153, 102, 255, 0.3)',
+          'rgba(255, 159, 64, 0.3)',
+          'rgba(255, 99, 132, 0.3)'
+        ],
+        hoverBorderWidth:5,
+        hoverBorderColor:[
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)'
+        ]
       }]
     },
     options:{
       title:{
         display:true,
         text:graphTitle2+" Graph of "+graphTitle +" (2014 - 2019)",
-        fontSize:25
+        fontSize:25,
+        responsive: true,
+        maintainAspectRatio: true
       },
       legend:{
         display:false,
@@ -69,7 +145,7 @@ for(var i =0;i<file.length;i++){
         padding:{
           left:0,
           right:0,
-          bottom:0,
+          bottom:padding,
           top:0
         }
       },
@@ -79,7 +155,3 @@ for(var i =0;i<file.length;i++){
     }
   });
 });
-
-String.prototype.replaceAt=function(index, replacement) {
-    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
-}
