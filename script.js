9b96ea6122b535e5c9148b32c29ffd07d0ee562a
Ki Rim <d.kim1423@gmail.com>
2019-09-24 22:54:54 +1200

    second commit

--- script.js
+++ script.js
@@ -3,36 +3,38 @@ fetch("data.json")
 .then(json =>{
 var keys = [];
 let selectedData = document.getElementById('selector').value;
+let graphTitle= selector.options[selector.selectedIndex].text;
+let graphTitle2= selector2.options[selector2.selectedIndex].text;
 console.log(selectedData);
 var file = eval("json.".concat(selectedData));
 console.log(file);
-for(var i = 0;i<file.length;i++)
-{
-  Object.keys(file[i]).forEach(function(key){
-      if(keys.indexOf(key) == -1)
-      {
-          keys.push(key);
-      }
-  });
+var label;
+if(graphTitle.includes("Watch")){
+  label = "Videos watched";
+}
+else{
+  label= "Searches made";
+}
+let selectedData2 = document.getElementById('selector2').value;
+console.log(selectedData2);
+
+
+var names = [];
+for(var i =0;i<file.length;i++){
+  names.push(file[i].name);
 }
 var values = [];
-for(var i = 0;i<file.length;i++)
-{
-  Object.values(file[i]).forEach(function(value){
-      if(values.indexOf(value) == -1)
-      {
-          values.push(value);
-      }
-  });
+for(var i =0;i<file.length;i++){
+  values.push(file[i].value);
 }
   let myChart = document.getElementById('myChart').getContext('2d');
   let dataChart = new Chart(myChart,{
 
-    type:'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+    type:selectedData2, // bar, horizontalBar, pie, line, doughnut, radar, polarArea
     data:{
-      labels:keys,
+      labels:names,
       datasets:[{
-        label:'Queries',
+        label:label,
         data:values,
         //backgroundColor:'green',
         backgroundColor:[
@@ -53,7 +55,7 @@ for(var i = 0;i<file.length;i++)
     options:{
       title:{
         display:true,
-        text:'David Youtube Data',
+        text:graphTitle2+" Graph of "+graphTitle +" (2014 - 2019)",
         fontSize:25
       },
       legend:{
@@ -65,10 +67,10 @@ for(var i = 0;i<file.length;i++)
       },
       layout:{
         padding:{
-          left:50,
+          left:0,
           right:0,
           bottom:0,
-          top:50
+          top:0
         }
       },
       tooltips:{
@@ -77,3 +79,7 @@ for(var i = 0;i<file.length;i++)
     }
   });
 });
+
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
